3419c4b590bff572efa0564f6f56c9021ec35303
John Medland <john@serenial.io>
2025-12-06 16:36:01 +0000

    basing toolchain on vpckg's linux one

--- vcpkg-overlay-triplets/arm32v7-linux-ci.cmake
+++ vcpkg-overlay-triplets/arm32v7-linux-ci.cmake
@@ -3,6 +3,10 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 
+# values from /usr/local/oecore-x86_64/environment-setup-cortexa9-vfpv3-nilrt-linux-gnueabi
+set(VCPKG_C_FLAGS "-march=armv7-a -mfpu=vfpv3  -mfloat-abi=softfp -mcpu=cortex-a9")
+set(VCPKG_CXX_FLAGS "${VCPKG_C_FLAGS}")
+
 # Reference the main toolchain file
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../scripts/toolchains/nilrt-arm32v7.cmake")
 
