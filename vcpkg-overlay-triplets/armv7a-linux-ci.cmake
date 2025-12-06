fa6546410840c6d18035c7aabc75099947b1e7d1
John Medland <john@serenial.io>
2025-12-06 22:37:48 +0000

    starting to fixup g_ar_toolkit code to GCC6

--- vcpkg-overlay-triplets/armv7a-linux-ci.cmake
+++ vcpkg-overlay-triplets/armv7a-linux-ci.cmake
@@ -4,10 +4,6 @@ set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 set(VCPKG_BUILD_TYPE release)
 
-# values from /usr/local/oecore-x86_64/environment-setup-cortexa9-vfpv3-nilrt-linux-gnueabi
-set(VCPKG_C_FLAGS "-march=armv7-a -mfpu=vfpv3  -mfloat-abi=softfp -mcpu=cortex-a9")
-set(VCPKG_CXX_FLAGS "-march=armv7-a -mfpu=vfpv3  -mfloat-abi=softfp -mcpu=cortex-a9")
-
 # Reference the main toolchain file
 set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/../scripts/toolchains/nilrt-armv7a.cmake")
 
