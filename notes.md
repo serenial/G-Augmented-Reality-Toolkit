587b5c74c44c2fa030d60eeb0f990902b832c669
John Medland <john@serenial.io>
2025-12-10 01:03:19 +0000

    integrating build tools into for armv7a

--- notes.md
+++ notes.md
@@ -3,3 +3,6 @@ https://web.archive.org/web/20201027083952/https://www.labviewmakerhub.com/doku.
 https://forums.ni.com/t5/NI-Linux-Real-Time-Discussions/Any-doc-on-hosting-my-own-OPKG-repository-How-about-how-to-make/td-p/3892635
 
 https://www.gernot-walzl.at/index.php?nav=.%2FDebian%2FRaspbian%2Fcross-compile
+
+https://downloads.raspberrypi.com/raspios_oldstable_lite_arm64/images/raspios_oldstable_lite_arm64-2025-11-24/2025-11-24-raspios-bookworm-arm64-lite.img.xz
+
--- scripts/toolchains/nilrt-armv7a.cmake
+++ scripts/toolchains/nilrt-armv7a.cmake
@@ -1,6 +1,9 @@
 # based on vcpkg/scripts/toolchains/linux.cmake
 # values from /usr/local/oecore-x86_64/environment-setup-cortexa9-vfpv3-nilrt-linux-gnueabi
 
+set(OE_INSTALL_SCRIPT oecore-x86_64-cortexa9-vfpv3-toolchain-6.0.sh)
+set(OE_INSTALL_SCRIPT_HASH SHA256=c6e7c560a662b31f45b4c323af9b81ab4bf301f1ccd02066dbe1550e940fcbaa)
+
 if(NOT _NILRT_ARM3V7_A_TOOLCHAIN)
     set(_NILRT_ARMV7_A_TOOLCHAIN 1)
 
@@ -9,9 +12,29 @@ if(NOT _NILRT_ARM3V7_A_TOOLCHAIN)
 
     set(CMAKE_CROSSCOMPILING ON)
 
+    include(FetchContent)
+
+    FetchContent_GetProperties(oe_armv7a_compile_tools)
+
+    if(NOT oe_armv7a_compile_tools_POPULATED)
+        message(NOTICE "-->> Downloading and installing cross compile tools for armv7 targets from NI.com...")
+
+        set(OE_COMPILE_TOOLS_DIR "${CMAKE_BINARY_DIR}/oe_armv7a_compile_tools")
+
+        FetchContent_Populate(
+            oe_armv7a_compile_tools
+            URL "https://download.ni.com/support/softlib/labview/labview_rt/2018/Linux%20Toolchains/linux/${OE_INSTALL_SCRIPT}"
+            URL_HASH "${OE_INSTALL_SCRIPT_HASH}"
+            DOWNLOAD_DIR "${OE_COMPILE_TOOLS_DIR}"
+            DOWNLOAD_NO_EXTRACT TRUE
+            UPDATE_COMMAND chmod +x "${OE_COMPILE_TOOLS_DIR}/${OE_INSTALL_SCRIPT}" && "${OE_COMPILE_TOOLS_DIR}/${OE_INSTALL_SCRIPT}" -y -d "${OE_COMPILE_TOOLS_DIR}"
+            PATCH_COMMAND echo "-->> Cross compile tools for armv7 targets installed!"
+        )
+    endif()
+
     # Sysroot configuration
-    set(SDKTARGETSYSROOT "/usr/local/oecore-x86_64/sysroots/cortexa9-vfpv3-nilrt-linux-gnueabi")
-    set(OECORE_NATIVE_SYSROOT "/usr/local/oecore-x86_64/sysroots/x86_64-nilrtsdk-linux")
+    set(SDKTARGETSYSROOT "${OE_COMPILE_TOOLS_DIR}/sysroots/cortexa9-vfpv3-nilrt-linux-gnueabi")
+    set(OECORE_NATIVE_SYSROOT "${OE_COMPILE_TOOLS_DIR}/sysroots/x86_64-nilrtsdk-linux")
 
     set(CMAKE_SYSROOT ${SDKTARGETSYSROOT})
     set(CMAKE_FIND_ROOT_PATH ${SDKTARGETSYSROOT})
@@ -35,9 +58,8 @@ if(NOT _NILRT_ARM3V7_A_TOOLCHAIN)
     # Compiler and Linker Flags
     set(CMAKE_C_FLAGS_INIT "-march=armv7-a -mfpu=vfpv3  -mfloat-abi=softfp -mcpu=cortex-a9 -fPIC")
     set(CMAKE_CXX_FLAGS_INIT "-march=armv7-a -mfpu=vfpv3  -mfloat-abi=softfp -mcpu=cortex-a9 -fPIC")
- 
+
     set(CMAKE_MODULE_LINKER_FLAGS_INIT "-Wl,-O1")
     set(CMAKE_SHARED_LINKER_FLAGS_INIT "-Wl,-O1")
     set(CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,-O1")
-
 endif()
