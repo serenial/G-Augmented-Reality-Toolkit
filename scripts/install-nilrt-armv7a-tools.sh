94fe13ee90894647244d06089381f8799493bf4f
John Medland <john@serenial.io>
2025-12-12 02:26:49 +0000

    fixup tool link

--- scripts/install-nilrt-armv7a-tools.sh
+++ scripts/install-nilrt-armv7a-tools.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 
-TOOLCHAIN_SCRIPT_SHA512="bfa9f07d8a024aa742eab81be4815156fec19f83aec35955cd8e559635f0503a481f6842ae65b6bd7d618a4dd177c1af9d9de4fa9f0fcf7f673263ae93c13239"
+TOOLCHAIN_SCRIPT_SHA512="31833d2ddc49b18d740332d9c2e8680ab3834b0f0a8e5555511323d6d6a819f01cd43725ad6c79c1571e47a0b23fa8209c367d40bb90b755fc896d51cf41cd7d"
 OPKG_UTILS_TAR_SHA512="441ee5ed416c3565617ae5fc413846ebc53e33876f9cce5e721afef2b8d9cd68723231ea12c8a2effaba2ccb33a36dc6e180994a57e6871f34d671c03ab36b7b"
 
-curl https://download.ni.com/support/softlib/labview/labview_rt/2018/Linux%20Toolchains/linux/oecore-x86_64-core2-64-toolchain-6.0.sh -Lo install-tc.sh
+curl https://download.ni.com/support/softlib/labview/labview_rt/2018/Linux%20Toolchains/linux/oecore-x86_64-cortexa9-vfpv3-toolchain-6.0.sh -Lo install-tc.sh
 echo "${TOOLCHAIN_SCRIPT_SHA512} install-tc.sh" | sha512sum --check --status
 chmod +x ./install-tc.sh
 sudo ./install-tc.sh -y
