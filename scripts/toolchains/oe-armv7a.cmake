7e02dd89b98763038e3e41723a2eefa54aa540c8
John Medland <john@serenial.io>
2026-09-15 22:24:47 +0100

    Use custom oe toolchain

--- scripts/toolchains/oe-armv7a.cmake
+++ scripts/toolchains/oe-armv7a.cmake
@@ -1,7 +1,8 @@
 # based on vcpkg/scripts/toolchains/linux.cmake
-# values from /usr/local/oecore-x86_64/environment-setup-cortexa9-vfpv3-nilrt-linux-gnueabi
-if(NOT _NILRT_ARM3V7_A_TOOLCHAIN)
-    set(_NILRT_ARM3V7_A_TOOLCHAIN TRUE)
+# values from usr/local/oecore-x86_64/environment-setup-armv7a-vfp-oe-linux-gnueabi
+
+if(NOT _OE_ARM3V7_A_TOOLCHAIN)
+    set(_OE_ARM3V7_A_TOOLCHAIN TRUE)
 
     set(CMAKE_SYSTEM_NAME Linux CACHE STRING "")
     set(CMAKE_SYSTEM_PROCESSOR armv7a-vfp)
@@ -12,18 +13,17 @@ if(NOT _NILRT_ARM3V7_A_TOOLCHAIN)
     set(OE_COMPILE_TOOLS_DIR "/usr/local/oecore-x86_64")
 
     # Sysroot configuration
-    set(SDKTARGETSYSROOT "${OE_COMPILE_TOOLS_DIR}/sysroots/cortexa9-vfpv3-nilrt-linux-gnueabi")
-    set(OECORE_NATIVE_SYSROOT "${OE_COMPILE_TOOLS_DIR}/sysroots/x86_64-nilrtsdk-linux")
+    set(SDKTARGETSYSROOT "${OE_COMPILE_TOOLS_DIR}/sysroots/armv7a-vfp-oe-linux-gnueabi")
+    set(OECORE_NATIVE_SYSROOT "${OE_COMPILE_TOOLS_DIR}/sysroots/x86_64-oesdk-linux")
     set(CMAKE_SYSROOT ${SDKTARGETSYSROOT})
     set(CMAKE_FIND_ROOT_PATH ${SDKTARGETSYSROOT})
 
     # Cross-compiler paths
-    set(CROSS_COMPILE_PREFIX "arm-nilrt-linux-gnueabi-")
-    set(TOOLCHAIN_BIN_DIR "${OECORE_NATIVE_SYSROOT}/usr/bin/arm-nilrt-linux-gnueabi")
+    set(CROSS_COMPILE_PREFIX "arm-oe-linux-gnueabi-")
+    set(TOOLCHAIN_BIN_DIR "${OECORE_NATIVE_SYSROOT}/usr/bin/arm-oe-linux-gnueabi")
 
-    #set(TOOLCHAIN_EXECUTABLE_PREFIX "${TOOLCHAIN_BIN_DIR}/${CROSS_COMPILE_PREFIX}")
+    set(TOOLCHAIN_EXECUTABLE_PREFIX "${TOOLCHAIN_BIN_DIR}/${CROSS_COMPILE_PREFIX}")
 
-    set(TOOLCHAIN_EXECUTABLE_PREFIX "/usr/bin/arm-linux-gnueabi-")
 
     set(CMAKE_C_COMPILER "${TOOLCHAIN_EXECUTABLE_PREFIX}gcc")
     set(CMAKE_CXX_COMPILER "${TOOLCHAIN_EXECUTABLE_PREFIX}g++")
@@ -41,10 +41,11 @@ if(NOT _NILRT_ARM3V7_A_TOOLCHAIN)
 
     # Compiler and Linker Flags
     set(CMAKE_CXX_STANDARD 17)
-    set(CMAKE_C_FLAGS_INIT "-march=armv7-a -mfpu=vfpv3 -mfloat-abi=softfp -O3 -fPIC")
-    set(CMAKE_CXX_FLAGS_INIT "-march=armv7-a -mfpu=vfpv3 -mfloat-abi=softfp -O3 -fPIC")
+    set(CMAKE_C_FLAGS_INIT "-march=armv7-a -mfpu=vfp -mfloat-abi=softfp -O3 -fPIC")
+    set(CMAKE_CXX_FLAGS_INIT "-march=armv7-a -mfpu=vfp -mfloat-abi=softfp -O3 -fPIC")
 
     set(CMAKE_MODULE_LINKER_FLAGS_INIT "-Wl,-O1")
     set(CMAKE_SHARED_LINKER_FLAGS_INIT "-Wl,-O1")
     set(CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,-O1")
+
 endif()
